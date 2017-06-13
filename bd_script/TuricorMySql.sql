9dfe277596e7456958ee559d54ec13ee730d2e4a
fdrcbrtl <federicojbertola@gmail.com>
2017-06-13 16:34:57 -0300

    Modifico script bd para que cree la bd y setee el charset a utf

--- bd_script/TuricorMySql.sql
+++ bd_script/TuricorMySql.sql
@@ -1,27 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jun 12, 2017 at 10:10 AM
--- Server version: 5.5.53-0+deb8u1
--- PHP Version: 5.6.29-0+deb8u1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `Turicor`
---
-
--- --------------------------------------------------------
-
+DROP DATABASE IF EXISTS Turicor;
+CREATE DATABASE Turicor CHARACTER SET utf8;
+USE Turicor;
 --
 -- Table structure for table `Cliente`
 --
@@ -31,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `Cliente` (
   `nombre` varchar(100) NOT NULL,
   `apellido` varchar(100) NOT NULL,
   `nro_documento` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -50,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `Reserva` (
   `id_vehiculo_ciudad` int(11) NOT NULL,
   `id_ciudad` int(11) NOT NULL,
   `id_pais` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -61,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `Reserva` (
 CREATE TABLE IF NOT EXISTS `Vendedor` (
 `id` int(11) NOT NULL,
   `nombre` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB;
 
 --
 -- Indexes for dumped tables
@@ -115,6 +94,3 @@ ALTER TABLE `Reserva`
 ADD CONSTRAINT `fk_vendedor_id` FOREIGN KEY (`id_vendedor`) REFERENCES `Vendedor` (`id`),
 ADD CONSTRAINT `fk_cliente_id` FOREIGN KEY (`id_cliente`) REFERENCES `Cliente` (`id`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
