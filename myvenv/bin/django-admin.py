e1afc197a2c73f49dd38ffadcdd8348999cfd207
fdrcbrtl <federicojbertola@gmail.com>
2017-07-01 13:25:32 -0300

    Creo una carpeta para cada proyecto

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ignacio/Documentos/Facu-2017/IAEW/practico/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ web_api/env_api/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fdrcbrtl/Documentos/UTN/2017/IAEW/TPIAEW/web_api/env_api/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
